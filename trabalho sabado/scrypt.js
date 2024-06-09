6f871ec1cdb9e40bcf32afc95349dd76eed86903
Ryreps <joaopedrocastroivo@gmail.com>
2024-06-08 23:38:43 -0300

    Add files via upload

---
+++ trabalho sabado/scrypt.js
@@ -0,0 +1,21 @@
+
+    document.getElementById('next1').onclick = function() {
+        document.getElementById('div1').style.display = 'none';
+        document.getElementById('div2').style.display = 'block';
+    };
+
+    document.getElementById('prev2').onclick = function() {
+        document.getElementById('div2').style.display = 'none';
+        document.getElementById('div1').style.display = 'block';
+    };
+
+    document.getElementById('next2').onclick = function() {
+        document.getElementById('div2').style.display = 'none';
+        document.getElementById('div1').style.display = 'block';
+    };
+
+    document.getElementById('prev1').onclick = function() {
+        document.getElementById('div1').style.display = 'none';
+        document.getElementById('div2').style.display = 'block';
+    };
+
